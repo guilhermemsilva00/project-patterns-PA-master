7818e555f166971d7d88dfbced24c6cf9f5234af
Guilherme Mendes <guilherme.mendes@dextra-sw.com>
2020-09-19 19:30:53 -0300

    exercicio-VideoPlayer

---
+++ TemplateMethodPattern1/main.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        VideoPlayer videoPlayer = null;
+        System.out.print("Input file name: ");
+        String filename = reader.readLine();
+
+        if (filename.indexOf("mkv") > 0) {
+          videoPlayer = new MkvPlayer(filename);
+        } else if (filename.indexOf("mp4") > 0) {
+          videoPlayer = new Mp4Player(filename);
+        } else {
+          throw new IllegalArgumentException(filename + " type not supported");
+        }
+
+
+        videoPlayer.playVideo();
+  }
+}
